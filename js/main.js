89a2ed925640658e5c69fee425a3f3153de9277b
VZalesinski <vzalesinski@gmail.com>
2021-06-02 19:50:41 +0300

    add 768 media

--- js/main.js
+++ js/main.js
@@ -9,7 +9,27 @@ const swiper = new Swiper('.swiper-container', {
   spaceBetween: 40,
   loop: true,
   loopedSlides: 3,
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 1,
+    },
+  }
 });
 
 // burger-menu
 
+const menuBurger = document.querySelector('.header__burger');
+if (menuBurger) {
+  const navList = document.querySelector('.nav__list');
+  menuBurger.addEventListener("click", function() {
+    document.body.classList.toggle('_lock');
+    menuBurger.classList.toggle('_active');
+    navList.classList.toggle('_active');
+  })
+}
